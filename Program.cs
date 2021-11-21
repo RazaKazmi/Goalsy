80a977c0bbe5ad5becbeecc6172d6c1c81d2dd18
Raza Kazmi <22304772+RazaKazmi@users.noreply.github.com>
2021-11-20 22:04:03 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Goalsy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
