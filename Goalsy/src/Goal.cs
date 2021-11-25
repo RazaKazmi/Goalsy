a825a3db24fbf1703dddd3b9542c8fedbbd18354
RazaKazmi <22304772+RazaKazmi@users.noreply.github.com>
2021-11-25 14:21:26 -0500

    Adds initial goal and task abstract classes.

---
+++ Goalsy/src/Goal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Goalsy.Components;
+
+namespace Goalsy.Objectives
+{
+    abstract class Goal : IObjective
+    {
+        private string _description;
+        protected List<IComponent> _components;
+        protected List<Task> _tasks;
+
+        public string Description 
+        { 
+            get => _description; 
+            // TO DO: Verify description is within character limit and valid
+            set => _description = value; 
+        }
+
+        public void AttachComponent(IComponent component)
+        {
+            _components.Add(component);
+        }
+
+        public void DetachComponent(IComponent component)
+        {
+            _components.Remove(component);
+        }
+
+        protected abstract void Init();
+    }
+}
---
+++ Goalsy/src/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Goalsy.Components;
+
+
+namespace Goalsy.Objectives
+{
+    abstract class Task : IObjective
+    {
+        private string _description;
+        protected List<IComponent> _components;
+        //protected List<Subtasks> _subtasks;
+
+        public string Description 
+        {
+            get => _description;
+            // TO DO: Verify description is within character limit and valid
+            set => throw new NotImplementedException(); 
+        }
+
+        public void AttachComponent(IComponent component)
+        {
+            _components.Add(component);
+        }
+
+        public void DetachComponent(IComponent component)
+        {
+            _components.Remove(component);
+        }
+    }
+}
