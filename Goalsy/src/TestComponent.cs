3985f709cea0a657865a42997ae5499295fcba36
RazaKazmi <22304772+RazaKazmi@users.noreply.github.com>
2021-11-25 19:49:42 -0500

    Adds temporary test goal and component.

---
+++ Goalsy/src/TestComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goalsy.Components
+{
+    public class TestComponent : IComponent
+    {
+        public string Name { get => "TestComponent" ; set => throw new NotImplementedException(); }
+    }
+}
---
+++ Goalsy/src/TestGoal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Goalsy.Components;
+
+namespace Goalsy.Objectives
+{
+    class TestGoal : Goal
+    {
+        public TestGoal(string name)
+        {
+            Init();
+            Description = name;
+        }
+
+        protected override void Init()
+        {
+            _components = new List<IComponent>();
+            _tasks = new List<Task>();
+        }
+    }
+}
