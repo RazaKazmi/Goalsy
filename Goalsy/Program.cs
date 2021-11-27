f05fbaba91bf276b9bb505035af0dab791dd21e9
RazaKazmi <22304772+RazaKazmi@users.noreply.github.com>
2021-11-26 19:03:17 -0500

    Adds .NET Generic Host. Generic Host confiures logging, depedency injection, and configs. Reconfigures application entry point. Adds serilog for logging.

--- Goalsy/Program.cs
+++ Goalsy/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.IO;
 using Goalsy.Objectives;
 using Goalsy.Components;
 
@@ -8,6 +14,55 @@ namespace Goalsy
     {
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            // Create and configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application starting");
+
+            // Create and configure host
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<Application>();
+                })
+                .UseSerilog()
+                .Build();
+
+            // Application entry point
+            var svc = ActivatorUtilities.CreateInstance<Application>(host.Services);
+            svc.Run();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    }
+
+    public class Application
+    {
+        private readonly ILogger<Application> _logger;
+        private readonly IConfiguration _config;
+
+        public Application(ILogger<Application> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public void Run()
+        {
+            _logger.LogInformation("Application started");
             Console.WriteLine("Welcome to Goalsy!");
             //CreateGoal("Get whiter teeth",0,0,0);
             //CreateGoal("Learn to whistle");
