d40bf5b3263041ff02aa65344e37922c84693aee
RazaKazmi <22304772+RazaKazmi@users.noreply.github.com>
2021-11-29 23:25:28 -0500

    Adds additonal methods to Priority class.

--- Goalsy/src/Priority.cs
+++ Goalsy/src/Priority.cs
@@ -5,7 +5,27 @@ namespace Goalsy.Components
 {
     internal class Priority : IComponent
     {
+        public enum PriorityLevel
+        {
+            Low = 1,
+            Medium,
+            High,
+            Critical
+        }
+
         public string Name => "PriorityComponent";
+        private PriorityLevel _priorityLevel;
+
+        PriorityLevel Level
+        {
+            get { return _priorityLevel; }
+            set { _priorityLevel = value; }
+        }
+
+        public Priority(PriorityLevel priorityLevel)
+        {
+            _priorityLevel = priorityLevel;
+        }
 
         public ComponentType GetComponentType()
         {
